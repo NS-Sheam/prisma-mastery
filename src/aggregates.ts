3d720e6aae0ca79c87b43439f4ae7b0d3728cfb8
NS-Sheam <123sheamfeni@gmail.com>
2024-03-12 11:48:14 +0600

    Update dev script and add age field to User model

---
+++ src/aggregates.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const aggregates = async () => {
+  // average age
+  const avgAge = await prisma.user.aggregate({
+    _avg: {
+      age: true,
+    },
+  });
+  // sum of age
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+  // count of age
+  const countAge = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
+  // find number of records
+  const countData = await prisma.user.count();
+
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+  console.log(minAge);
+};
+
+aggregates();
